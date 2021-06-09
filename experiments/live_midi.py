dce82f3f5b459fe77ea637d4ce4c5f12ba6df54c
Benito Buchheim <github.benito.2017@buchheims.de>
2021-06-09 18:11:23 +0200

    live midi to logic pro

---
+++ experiments/live_midi.py
@@ -0,0 +1,13 @@
+# Experimenting with using Python as a midi device
+
+import mido
+import time
+
+output = mido.open_output('My Output Device', virtual=True)
+
+c_major = [60, 62, 64, 65, 67, 69, 71, 72]
+
+for note in c_major:
+    output.send(mido.Message('note_on', note=note))
+    time.sleep(0.5)
+    output.send(mido.Message('note_off', note=note))
