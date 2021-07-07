e0355f6ea7eb3a13f1fafe4b677b41681a9789f8
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2021-07-07 18:05:20 +0200

    implement some scenes
    Co-authored-by: benbuc <benito.buchheim@student.hpi.de>

---
+++ README.md
@@ -0,0 +1,36 @@
+
+Scenes
+======
+- overcast
+  - thunderstorm
+  - intense_rain
+  - rainy (drizzle)
+    - hot
+    - cold
+  - windy
+- broken
+  - misty
+  - gusty
+  - rainy
+  - rainbow
+  - approaching
+- scattered (< 50% clouds)
+  - clearing
+  - shower
+  - light_precipitation
+  - windy
+  - hot
+  - sweltry
+- clear (sunny, < 15% clouds)
+  - broiling
+  - sweltry
+  - nice
+  - hazy
+  - chilly
+- night
+  - chilly
+  - warm
+  - thunderstorm
+  - stormy
+
+https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
--- components/SceneParser.py
+++ components/SceneParser.py
@@ -1,8 +1,70 @@
 from enum import Enum, auto
+import logging
 
 class Scene(Enum):
-    THUNDERSTORM = auto()
-    MILD_SUMMER = auto()
+    ANY = auto()
+    OVERCAST_THUNDERSTORM = auto()
+    OVERCAST_WINDY = auto()
+    BROKEN_RAINY = auto()
+    BROKEN_GUSTY = auto()
+    SCATTERED_WINDY = auto()
+    SCATTERED_SWELTRY = auto()
+    CLEAR_BROILING = auto()
+    CLEAR_NICE = auto()
+    NIGHT_CHILLY = auto()
+    NIGHT_WARM = auto()
+
+def match_scene(hour, data):
+    if hour < 5 or hour > 23:
+        if data['temperature'] < 5:
+            return Scene.NIGHT_CHILLY
+        elif data['temperature'] < 12 and data['wind'] > 8:
+            return Scene.NIGHT_CHILLY
+
+        if data['temperature'] > 22:
+            return Scene.NIGHT_WARM
+
+        logging.warn('night but not chilly and not warm')
+    
+    if data['clouds'] > 98:
+        if data['weather_condition'] > 200 and data['weather_condition'] < 300:
+            return Scene.OVERCAST_THUNDERSTORM
+
+        if data['wind'] > 8:
+            return Scene.OVERCAST_WINDY
+
+        logging.warn('overcast but no thundering and no wind')
+
+    elif data['clouds'] > 50:
+        if data['rain'] > 0:
+            return Scene.BROKEN_RAINY
+
+        if data['wind'] > 8:
+            return Scene.BROKEN_GUSTY
+
+        logging.warn('broken but no rain and no wind')
+
+    elif data['clouds'] > 15:
+        if data['humidity'] > 80 and data['temperature'] > 24:
+            return Scene.SCATTERED_SWELTRY
+
+        if data['wind'] > 8:
+            return Scene.SCATTERED_WINDY
+
+        logging.warn('scattered but not sweltry and no wind')
+
+    else:
+        if data['temperature'] > 32:
+            return Scene.CLEAR_BROILING
+        elif data['temperature'] > 26 and data['wind'] < 1:
+            return Scene.CLEAR_BROILING
+
+        if data['temperature'] > 16 and data['rain'] == 0:
+            return Scene.CLEAR_NICE
+
+        logging.warn('clear but not broiling and not nice')
+        
+    return Scene.ANY
 
 class SceneParser:
     def __init__(self, weather_data):
@@ -10,10 +72,9 @@ class SceneParser:
 
     def get_scenes(self):
         scenes = []
-        for i in range(24):
-            if self.weather_data['rain_percentage'][i] > 0.5:
-                scenes.append(Scene.THUNDERSTORM)
-            else:
-                scenes.append(Scene.MILD_SUMMER)
+        for hour in range(24):
+            hour_data = {key:value[hour] for (key,value) in self.weather_data.items()}
+
+            scenes.append(match_scene(hour, hour_data))
 
         return scenes
