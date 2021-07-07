320e3c6b8053fb3d9bad25fedb1d8cb8c3afc400
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2021-07-07 18:05:55 +0200

    refactor track generation
    Co-authored-by: benbuc <benito.buchheim@student.hpi.de>

--- music/Track.py
+++ music/Track.py
@@ -21,19 +21,14 @@ class Track:
         track.append(mido.Message('program_change', program=self.program, time=0))
         track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(Meter.bpm)))
 
-        notes_i = 0
         last_message_time = 0
 
-        for i in range(Meter.total_beats() * Meter.max_subdivs):
-            while len(self.notes) >= notes_i+1 and self.notes[notes_i][0] == i:
-                current_time = i * (Meter.ticks_per_beat // Meter.max_subdivs)
-                time_diff = current_time - last_message_time
-                last_message_time = current_time
+        for note in self.notes:
+            current_time = note[0] * (Meter.ticks_per_beat // Meter.max_subdivs)
+            time_diff = current_time - last_message_time
+            last_message_time = current_time
 
-                note = self.notes[notes_i]
-                msg = mido.Message(note[2], note=note[1], velocity=127, time=time_diff)
-                track.append(msg)
-
-                notes_i += 1
+            msg = mido.Message(note[2], note=note[1], velocity=127, time=time_diff)
+            track.append(msg)
 
         return track
