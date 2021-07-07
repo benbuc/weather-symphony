40fe24752f895944557f080f6764f20c93152576
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2021-07-07 18:06:36 +0200

    add file loader for openweathermap API responses
    Co-authored-by: benbuc <benito.buchheim@student.hpi.de>

--- data_loaders/__init__.py
+++ data_loaders/__init__.py
@@ -1,1 +1,2 @@
 from .dummy_loader import DummyLoader
+from .api_file_loader import APIFileLoader
---
+++ data_loaders/api_file_loader.py
@@ -0,0 +1,23 @@
+import json
+
+from .DataLoader import DataLoader
+
+class APIFileLoader(DataLoader):
+    def __init__(self, file, date):
+        super().__init__(date)
+        self.file = file
+
+    def get_weather_data(self):
+        json_data = json.load(open(self.file, "r"))
+        hourly_data = json_data['hourly']
+
+        return {
+            'temperature': [(hour['feels_like'] - 273.15) for hour in hourly_data],
+            'humidity': [hour['humidity'] for hour in hourly_data],
+            'clouds': [hour['clouds'] for hour in hourly_data],
+            'wind': [hour['wind_speed'] for hour in hourly_data],
+            'visibility': [hour['visibility'] for hour in hourly_data],
+            'weather_condition': [hour['weather'][0]['id'] for hour in hourly_data],
+            'rain': [hour['rain']['1h'] if ('rain' in hour) else 0 for hour in hourly_data],
+            'snow': [hour['snow']['1h'] if ('snow' in hour) else 0 for hour in hourly_data],
+        }
