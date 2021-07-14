407d1e6bb2972153095e742882f7b91a7d65e970
Benito Buchheim <benito.buchheim@student.hpi.de>
2021-07-14 14:57:52 +0200

    harmony generator works reverse and always ends on tonic

--- weather_symphony/components/harmony_generator.py
+++ weather_symphony/components/harmony_generator.py
@@ -11,23 +11,18 @@ class HarmonyGenerator:
 
     def get_harmony(self):
 
-        chords = []
-        keys = []
-        last = (1, "maj")
-        for bar_num in range(len(self.weather_data) * Meter.bars_per_hour):
-            if last == (1, "maj"):
-                possible_nexts = list(chord_graph_major.keys())
-            else:
-                possible_nexts = [last] + chord_graph_major[last]
+        inverted_chord_graph = invert_graph(chord_graph_major)
 
-            sampled_next = possible_nexts[randint(0, len(possible_nexts) - 1)]
-            chords.append(sampled_next)
-            last = sampled_next
+        total_bars = len(self.weather_data) * Meter.bars_per_hour
+        chords = [None] * (total_bars - 1) + [(1, "maj")]
+        keys = [None] * (total_bars - 1) + [Major(24)]
 
-            if bar_num > 10:
-                keys.append(Major(26))
-            else:
-                keys.append(Major(24))
+        for bar_num in reversed(range(total_bars - 1)):
+            possible_prev = [(1, "maj")] + inverted_chord_graph[chords[bar_num + 1]]
+
+            sampled_prev = possible_prev[randint(0, len(possible_prev) - 1)]
+            chords[bar_num] = sampled_prev
+            keys[bar_num] = keys[bar_num + 1]
 
         return {
             "keys": keys,
@@ -43,3 +38,12 @@ chord_graph_major = {
     (5, "maj"): [(1, "maj")],
     (6, "min"): [(5, "maj")],
 }
+
+
+def invert_graph(graph):
+    new_graph = {key: [] for key in graph.keys()}
+    for key in graph.keys():
+        for target in graph[key]:
+            new_graph[target].append(key)
+
+    return new_graph
