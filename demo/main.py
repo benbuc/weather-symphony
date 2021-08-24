1e2559aad4a62114cab1a5b1403d0218e1f7f2dd
ShirleyNekoDev <maximilian.stiede@student.hpi.de>
2021-08-24 20:18:38 +0200

    add weather API cache for demo server

--- demo/main.py
+++ demo/main.py
@@ -1,11 +1,14 @@
 import asyncio
-import datetime
 import json
+import os.path
+from datetime import date
 
-from fastapi import FastAPI
+from aiohttp import ClientError, ClientSession, ClientTimeout, DummyCookieJar
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, StreamingResponse
 
+from demo.open_weather_api import get_weather_data
 from weather_symphony.main import get_mido
 
 app = FastAPI()
@@ -18,6 +21,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+session: ClientSession = None
+
+
+@app.on_event("startup")
+async def startup_event():
+    timeout = ClientTimeout(connect=4, total=16)
+    cookie_jar = DummyCookieJar()
+    global session
+    session = await ClientSession(timeout=timeout, cookie_jar=cookie_jar).__aenter__()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await asyncio.sleep(0)
+    global session
+    if session is not None:
+        await session.__aexit__(None, None, None)
+        session = None
+
 
 @app.get("/")
 async def root():
@@ -25,15 +47,58 @@ async def root():
 
 
 @app.get("/api/")
-async def api():
-    def progressReportPacket(step_name, progress=None):
-        return json.dumps({"step": step_name, "progress": progress}) + "\n\n"
+async def api(
+    date_string: str = Query(..., regex="^\\d{4}-\\d{2}-\\d{2}$"),
+    latitude: str = Query(
+        ...,
+        regex="^(\\+|-)?(?:90(?:(?:\\.0{1,8})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,8})?))$",  # noqa: E501
+    ),
+    longitude: str = Query(
+        ...,
+        regex="^(\\+|-)?(?:180(?:(?:\\.0{1,8})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,8})?))$",  # noqa: E501
+    ),
+    seed: str = Query(0, max_length=64),
+    apiKey: str = Query(..., regex="^[0-9a-f]{32}$"),
+):
+    latitude: float = round(float(latitude) * 1000) / 1000
+    longitude: float = round(float(longitude) * 1000) / 1000
+    date_obj = date.fromisoformat(date_string)
+
+    def progressReportPacket(step_name, progress=None, from_cache=False, failed=False):
+        return (
+            json.dumps(
+                {
+                    "step": step_name,
+                    "progress": progress,
+                    "cached": from_cache,
+                    "failed": failed,
+                }
+            )
+            + "\n\n"
+        )
 
     async def iterfile():
         yield progressReportPacket("Call Weather API", progress=None)
-        await asyncio.sleep(0.5)
+        global session
+        filename = f"cache/{latitude}_{longitude}_{date}.json"
+        if os.path.isfile(filename):
+            print("request cached=", filename)
+            yield progressReportPacket(
+                "Call Weather API", progress=100, from_cache=True
+            )
+        else:
+            try:
+                await get_weather_data(
+                    session, filename, date, latitude, longitude, apiKey
+                )
+            except ClientError as ex:
+                print("api call failed=", ex)
+                yield progressReportPacket(
+                    "Call Weather API", progress=None, failed=True
+                )
+                return
         yield progressReportPacket("Generate MIDI", progress=None)
-        get_mido(datetime.date.today(), 0)  # TODO
+        get_mido(date_obj, 0)  # TODO
         await asyncio.sleep(1.5)
         yield progressReportPacket("Convert to audio", progress=0)
         await asyncio.sleep(1.5)
---
+++ demo/open_weather_api.py
@@ -0,0 +1,43 @@
+import calendar
+from datetime import date
+
+from aiohttp import ClientResponseError, ClientSession
+
+chunk_size = 4096
+
+
+async def get_weather_data(
+    session: ClientSession,
+    filename: str,
+    date_string: str,
+    latitude: str,
+    longitude: str,
+    apiKey: str,
+):
+    utc_timestamp = calendar.timegm(date.fromisoformat(date_string).timetuple())
+    url = "http://api.openweathermap.org/data/2.5/onecall/timemachine"
+    params = {"lat": latitude, "lon": longitude, "dt": utc_timestamp, "appid": apiKey}
+    print("querying api=", url, params)
+    async with session.get(
+        url,
+        params=params,
+        headers={"Accept": "application/json"},
+        allow_redirects=False,
+    ) as response:
+        content_type = response.headers["content-type"].split(";")[0].strip()
+        if (not response.ok) or (content_type != "application/json"):
+            raise ClientResponseError(
+                request_info=response.request_info,
+                status=response.status,
+                message="API call failed",
+                headers=response.headers,
+                history=response.history,
+            )
+        else:
+            print("writing into cache=", filename)
+            with open(filename, "xb") as fd:
+                while True:
+                    chunk = await response.content.read(chunk_size)
+                    if not chunk:
+                        break
+                    fd.write(chunk)
